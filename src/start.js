47510a45691de96b218d34b546c4142c31237ad2
Etienne <etienne.rousseau@pampatech.net>
2023-07-07 20:58:42 -0700

    First Iteration

---
+++ src/start.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './StartPage.css';
+
+const StartPage = () => {
+  return (
+    <div className="start-page">
+      <div className="container">
+        <h1 className="name">Etienne Rousseau</h1>
+        <p className="introduction">Web Developer &amp; Designer</p>
+        <div className="social-links">
+          <a href="https://www.linkedin.com/in/etienne-rousseau" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/etienne-rousseau" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://twitter.com/etienne_rousseau" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StartPage;
